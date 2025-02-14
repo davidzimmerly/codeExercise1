b92b50060e618c4c081ce1dc633627cf93b0b2bd
David Zimmerly <davidazimmerly@gmail.com>
2025-02-13 19:42:14 -0600

    added cardviews

--- app/src/main/java/com/example/codeexercise1/composables/ItemScreen.kt
+++ app/src/main/java/com/example/codeexercise1/composables/ItemScreen.kt
@@ -1,11 +1,23 @@
 package com.example.codeexercise1.composables
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.codeexercise1.util.serviceObjects.Item
 import com.example.codeexercise1.viewModel.MainViewModel
@@ -20,14 +32,72 @@ fun ItemScreen(
 }
 
 @Composable
-fun ItemListView(modifier: Modifier, items: Map<Int, List<Item>>) =
+fun ItemListView(modifier: Modifier, items: Map<Int, List<Item>>) {
     LazyColumn(modifier = modifier) {
         items.forEach { (listId, itemList) ->
             item {
-                Text(text = "List ID: $listId")
+                Text(
+                    text = "List ID: $listId",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    color = MaterialTheme.colorScheme.primary,
+                    textAlign = TextAlign.Center
+                )
             }
             items(itemList) { item ->
-                Text(text = "List ID: ${item.listId}, Name: ${item.name}, Item ID: ${item.id}")
+                ItemCard(item = item)
             }
         }
     }
+}
+
+@Composable
+fun ItemCard(item: Item) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp,
+            pressedElevation = 8.dp,
+            hoveredElevation = 6.dp,
+            focusedElevation = 6.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "${item.name}",
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.weight(3f)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))  // Space between texts
+
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "Item ID: ${item.id}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+                Text(
+                    text = "List ID: ${item.listId}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+            }
+        }
+    }
+}
+
+
